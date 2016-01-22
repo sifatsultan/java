ccc5b6dca9bebadb38f1b6ede1d03d81105d4501
Sifat Sultan <sifatsultan@gmail.com>
2016-01-22 22:49:27 +0800

    did commenting

--- src/com/sifat/Main.java
+++ src/com/sifat/Main.java
@@ -11,22 +11,24 @@ public class Main {
 
         int[] nameLength = new int[names.length];
 
+        //map name array to length array
         for (int i = 0; i < names.length; i++) {
             nameLength[i] = names[i].length();
         }
 
-        
+        //find max length in the array
         int max = nameLength[0];
         for (int i = 0; i < nameLength.length; i++) {
             if(nameLength[i]>max) max = nameLength[i];
         }
 
-
+        //find position of max
         int maxPosition = 0;
         for (int i = 0; i < nameLength.length; i++) {
             if(nameLength[i] == max) maxPosition = i;
         }
 
+        //get name of the position
         System.out.print("Long name: "+names[maxPosition]);
     }
 }
